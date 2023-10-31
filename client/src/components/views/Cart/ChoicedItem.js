997bce3a60f83d18e271f53bc7fe63177df6ad0c
100opa <tmdwnsrhc@gmail.com>
2023-10-31 11:10:38 +0900

    삭제기능 테스트

--- client/src/components/views/Cart/ChoicedItem.js
+++ client/src/components/views/Cart/ChoicedItem.js
@@ -1,24 +1,37 @@
 const ChoicedItem = (props) => {
-  const productKey = props.data.key
-  const product = props.data.value
+  const itemSet = props.data;
+  const productKey = props.data.key;
+  const product = props.data.value;
+
+  const deleteCart = () => {
+    const cartData = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const newData = cartData.filter(item => item.key !== itemSet.key);
+    localStorage.setItem('cart', JSON.stringify(newData));
+
+    // //  // 중복 확인
+    // const isDuplicate = cartData.some(item => item.key === itemSet.key);
+
+    // if (!isDuplicate) {
+    //   // 중복되지 않으면 추가
+    //   cartData.push(itemSet);
+    //   localStorage.setItem('cart', JSON.stringify(cartData));
+    // } else {
+    //   alert("장바구니에 이미 존재하는 상품입니다.")
+    // }
+  };
+
   return (
     <li>
-      <img src={`./img/${productKey}.jpg`} alt={product.name}/>
+      <img src={`./img/${productKey}.jpg`} alt={product.name} />
       <div className="item-info">
         <p>{product.name}</p>
         <div className="price-box">
-          <span>판매가 : {product.discountPrice}</span>
-          <div className="quantity-box">
-            <label htmlFor="quantity1">수량</label>
-            <select name="quantity1" id="quantity1">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-          </div>
+          <span>판매가 : {product.price}</span>
+          <span>할인적용가 : {product.discountPrice}</span>
         </div>
       </div>
-      <button type="button" className="cancle-btn">
+      <button type="button" className="cancle-btn" onClick={deleteCart}>
         <img src="./img/cancle.svg" alt="cancle" />
       </button>
     </li>
--- client/src/components/views/Commons/ProductBox.js
+++ client/src/components/views/Commons/ProductBox.js
@@ -5,23 +5,23 @@ const ProductBox = (props) => {
 
   const addCart = () => {
     const cartData = JSON.parse(localStorage.getItem('cart')) || [];
-    
+
     //  // 중복 확인
     const isDuplicate = cartData.some(item => item.key === itemSet.key);
-    
+
     if (!isDuplicate) {
       // 중복되지 않으면 추가
       cartData.push(itemSet);
       localStorage.setItem('cart', JSON.stringify(cartData));
       alert("장바구니에 해당 상품이 추가되었습니다.")
     } else {
-      alert("장바구니에 이미 존재하는 상품입니다. \n수량 조정은 장바구니 페이지에서 가능합니다.")
+      alert("장바구니에 이미 존재하는 상품입니다.")
     }
   };
 
   return (
     <div href="#" className="product-box">
-      <a href="#"><img src={`./img/${imgName}.jpg`} alt="LG전자 오브제 4도어 냉장고"/></a>
+      <a href="#"><img src={`./img/${imgName}.jpg`} alt="LG전자 오브제 4도어 냉장고" /></a>
       <a href="#"><span className="product-name">{product.name}</span></a>
       <p className="discount-price"><span>{product.discountRate}</span>{product.discountPrice}</p>
       <span className="price">{product.price}</span>
